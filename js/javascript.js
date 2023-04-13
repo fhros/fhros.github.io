e967657d37b1a63837ec16b0b7d2c141f79d95f4
fhros <hanki.wifi@gmail.com>
2023-04-13 09:14:35 +0300

    cheese update

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById("btn")
+btn.onclick = func
+
+function func() 
+{
+    var a = true
+    while (a == true)
+         {
+          alert("olet kuin hiiri, liian keskittynyt juustoon niin unohdit ansan")
+         }
+    }
